57392e06284d78e6d0f13b0fb431256c743698a0
quietvoid <39477805+quietvoid@users.noreply.github.com>
2022-12-04 20:04:08 -0500

    Day 04

--- 2022/src/day04.rs
+++ 2022/src/day04.rs
@@ -1,9 +1,12 @@
+use std::ops::RangeInclusive;
+
 use itertools::Itertools;
 
-const INPUT_STR: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/inputs/day04-ex.txt"));
+const INPUT_STR: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/inputs/day04.txt"));
 
 #[derive(Default)]
 struct Day04 {
+    pairs: Vec<(RangeInclusive<usize>, RangeInclusive<usize>)>,
 }
 
 pub fn solve() {
@@ -12,14 +15,42 @@ pub fn solve() {
 
 impl Day04 {
     fn p1(mut self) -> Self {
-        let res = 0;
+        self.pairs = INPUT_STR
+            .lines()
+            .filter_map(|line| {
+                line.split(',')
+                    .filter_map(|sections| {
+                        sections
+                            .split('-')
+                            .map(|e| e.parse::<usize>().unwrap())
+                            .collect_tuple()
+                    })
+                    .map(|(start, end)| start..=end)
+                    .collect_tuple()
+            })
+            .collect();
+
+        let res = self
+            .pairs
+            .iter()
+            .filter(|pair| {
+                (pair.0.start() <= pair.1.start() && pair.0.end() >= pair.1.end())
+                    || (pair.1.start() <= pair.0.start() && pair.1.end() >= pair.0.end())
+            })
+            .count();
+
         println!("Part 1: {}", res);
 
         self
     }
 
     fn p2(self) {
-        let res = 0;
+        let res = self
+            .pairs
+            .iter()
+            .filter(|pair| pair.0.clone().any(|e| pair.1.contains(&e)))
+            .count();
+
         println!("Part 2: {}", res);
     }
 }
